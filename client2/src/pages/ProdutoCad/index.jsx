ca738adc167c703e9a5b0babeb3b98eb08bc3600
Marcosdsj <mfsilva5585@gmail.com>
2023-06-09 00:41:16 -0300

    add styled-components

--- client2/src/pages/ProdutoCad/index.jsx
+++ client2/src/pages/ProdutoCad/index.jsx
@@ -1,21 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 
-//import { Table } from "./styles";
+import { Container, titulo, AlertaSucess, AlertaDanger } from "./styles";
+//import "./styles.css";
 
 const ProdutoCad = () => {
-   /** "name": "PC samsung",
-	"preco": 120,
-	"disponivel": false,
-	"idUser": 2
+   /** "name": "PC samsung", preco": 120, 	"disponivel": false,	"idUser": 2     */
+   // preenchendo a constante produto dos valores da form
+    const [produto, setProduto] = useState({
+        name: '',
+        preco:  '',
+        disponivel: '',
+        idUser: ''        
+    });
+
+    // Respostas recebidas
+    const [status, setStatus] = useState({
+        type: '',
+        mensagem: ''
+    })
+    //set os novos valores de produtos do form
+   const  valorInput = e => setProduto({ ...produto, [e.target.name]: e.target.value}); //{...produto,} - vai recuperar os valore dos campos do produto
+
+    const cadProduto = async e =>{
+        e.preventDefault();
+        console.log("produto antes de enviou:", produto);
+        await fetch("http://localhost:7000/inserirproduto", {
+            method: 'POST',
+            headers: {                
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({...produto})
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            //console.log("resposta json: ",responseJson)
+            if(responseJson.erro){
+                setStatus({
+                    type: 'erro',
+                    mensagem: responseJson.mensagem
+                });
+            }else{
+                setStatus({
+                    type: 'sucess',
+                    mensagem: responseJson.mensagem
+                });
+            }           
+        })
+        .catch(() => {
+                setStatus({
+                    type: 'erro',
+                    mensagem: 'Produto não cadastrado, Tente mais tarde !'
+                });
+        });
+    }
+    /**
+     * fetch('https://www.meusite.com.br/api', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
      */
 
     return (
-        <div >
+        <div id="cadastro" >
             <title>Cadastrar produto</title>
-            <h1>cadastrar produto555</h1>
-            <form action="">
-                <label htmlFor="">Nome: </label>
-                <input type="text" name="name" placeholder="Nome do produto"/><br/><br/>
+            <h1 className="title" >Cadastrar produto</h1>
+            {status.type === 'erro'? <p>{status.mensagem }</p> : ""}
+            {status.type === 'sucess'? <p>{status.mensagem }</p> : ""}
+            <form className="form" onSubmit={cadProduto}>
+                <label >Nome: </label>
+                <input type="text" name="name" placeholder="Nome do produto" onChange={valorInput}/><br/><br/>
+
+                <label >Preço: </label>
+                <input type="number" name="preco" placeholder="Preço do produto" onChange={valorInput}/><br/><br/>
+
+                <label >Disponivel: </label>
+                <input type="number" name="disponivel" placeholder="disponibilidade do produto" onChange={valorInput}/><br/><br/>
+
+                <label >Proprietario: </label>
+                <input type="number" name="idUser" placeholder="Proprietario do produto" onChange={valorInput}/><br/><br/>
+
+                <button type="submit">Cadastrar</button>
 
             </form>
             
--- client2/src/pages/ProdutoCad/styles.js
+++ client2/src/pages/ProdutoCad/styles.js
@@ -1,11 +1,34 @@
-//import styled from 'styled-components';
-import styled from 'styled';
+import styled from 'styled-components';
+
+// criou conteniner para estilizar tudo
+export const Container = styled.section`
+        max-width: 960px;
+        margin: 20px auto;
+        box-shadow: 0 0 1em #6c757d;   
+`;
 
 export const titulo = styled.h1`
         color: #3e3e3e;
         font-size: 23px;
-        padding: 10px
-   
+        padding: 10px   
+`;
+//exibir mensagens
+export const AlertaSucess = styled.p`
+        background-color: #d1e7dd;
+        color: #0f5132;
+        margin: 20px 0;
+        boder: 1px solid #badbcc;
+        boder-radius: 4px;
+        padding: 7px
+`;
+
+export const AlertaDanger = styled.p`
+        background-color: #f8d7da;
+        color: #842029;
+        margin: 20px 0;
+        boder: 1px solid #f5c2c7;
+        boder-radius: 4px;
+        padding: 7px
 `;
 
 export const Table = styled.table`
